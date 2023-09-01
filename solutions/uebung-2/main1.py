a07a4388d62e74cbeaed80764b90675c90bcb6cd
Roland Lötscher <roland_loetscher@hotmail.com>
2023-09-01 07:52:07 +0200

    Füge Lösungen zu Übung 1 hinzu

---
+++ solutions/uebung-2/main1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Schreiben Sie ein Programm mit folgender Eingabe/Ausgabe 
+Eingabe: Vorname, Nachname, Geschlecht (m/f)
+Ausgabe: "Guten Tag Herr ... ..." bzw. "Guten Tag Frau ... ..." (mit Vorname und Nachname)
+
+Verwenden Sie dabei f-string Formattierung bei der Ausgabe und benützen Sie dabei
+Variablen vorname, nachname, anrede von Typ str
+"""
+
+vorname = input("Geben Sie Ihren Vornamen ein: ")
+nachname = input("Geben Sie Ihren Nachnamen ein: ")
+geschlecht = input("Geben Sie Ihr Geschlecht (m/f) ein: ")
+
+anrede = "Herr" if geschlecht == "m" else "Frau"
+print(f"Guten Tag {anrede} {vorname} {nachname}")
