08e350748073d6fa129d0843e902f6a7209dc255
Roland Lötscher <roland_loetscher@hotmail.com>
2024-04-12 08:47:00 +0200

    Vererbung: Beitrag B

--- solutions/uebung-vererbung/CONTRIBUTIONS.md
+++ solutions/uebung-vererbung/CONTRIBUTIONS.md
@@ -1,11 +1,11 @@
 ﻿# Autoren
 
-Bei diesem Projekt wirken 3 Personen mit, nämlich Anna, B und C.
+Bei diesem Projekt wirken 3 Personen mit, nämlich Anna, Bert und C.
 
 # Aufgabenaufteilung:
 
 Hier sind die Aufgaben, welche die einzelnen Mitwirkenden übernehmen, kurz beschrieben:
 
 - Anna: Schreibt Entity Klasse
-- B:
+- Bert: Schreibt Ghost Klasse
 - C:
--- solutions/uebung-vererbung/Ghost.py
+++ solutions/uebung-vererbung/Ghost.py
@@ -9,3 +9,23 @@ Klasse "Ghost" mit Basisklasse "Entity" und zusätzlichen
 - Methoden:      choose_dir: gibt eine zufällige Richtung "Up", "Down", "Left" oder "Right" zurück
                  distance:   berechnet die geometrische Distanz, welche den Geist aktuell von seinem Ausgangsfeld trennt
 """
+from Entity import Entity
+import random
+import math
+
+class Ghost(Entity):
+    def __init__(self, home_x, home_y):
+        super().__init__(0, 0, "Right")
+        self.home_x = home_x
+        self.home_y = home_y
+
+    def choose_dir(self):
+        return random.choice(["Up", "Down", "Left", "Right"])
+
+    def distance(self):
+        return math.sqrt((self.home_x - self.x)**2 + (self.home_y - self.y)**2)
+
+if __name__ == '__main__':
+    ghost = Ghost(2, 3)
+    print(f"Random direction: {ghost.choose_dir()}")
+    print(f"Distance from home: {ghost.distance()}")
